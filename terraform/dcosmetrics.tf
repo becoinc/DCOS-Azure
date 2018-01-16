992089f451289d14835e0de49865dc68542abb4d
Jeffrey Zampieron <jzampieron@zproject.net>
2018-01-16 11:52:17 -0500

    Add metrics plugin to large private agents

--- terraform/dcosmetrics.tf
+++ terraform/dcosmetrics.tf
@@ -181,3 +181,61 @@ resource "null_resource" "dcos_metrics_prometheus_private" {
     }
 
 }
+
+resource "null_resource" "dcos_metrics_prometheus_large_private" {
+
+    depends_on = [
+        "azurerm_virtual_machine.dcosLargePrivateAgent"
+    ]
+
+    count = "${azurerm_virtual_machine.dcosLargePrivateAgent.count}"
+
+    connection {
+        type         = "ssh"
+        host         = "${azurerm_network_interface.dcosLargePrivateAgentIF0.*.private_ip_address[ count.index ]}"
+        user         = "${var.vm_user}"
+        timeout      = "120s"
+        private_key  = "${file(var.private_key_path)}"
+        # Configuration for the Jumpbox
+        bastion_host        = "${azurerm_public_ip.dcosBootstrapNodePublicIp.ip_address}"
+        bastion_user        = "${var.vm_user}"
+        bastion_private_key = "${file(var.bootstrap_private_key_path)}"
+    }
+
+    # provisioners execute in order.
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mkdir -p /opt/dcos",
+            "sudo chown ${var.vm_user} /opt/dcos",
+            "sudo chmod 755 -R /opt/dcos"
+        ]
+    }
+
+    # Provision the files.
+    provisioner "file" {
+        source      = "${path.module}/files/dcos-metrics-prometheus.env"
+        destination = "/opt/dcos/dcos-metrics-prometheus.env"
+    }
+
+    provisioner "file" {
+        source      = "${path.module}/files/dcos-metrics-prometheus-agent.service"
+        destination = "/opt/dcos/dcos-metrics-prometheus-agent.service"
+    }
+
+    provisioner "file" {
+        source      = "${path.module}/files/dcos-metrics-prometheus-plugin_1.10.2"
+        destination = "/opt/dcos/dcos-metrics-prometheus-plugin"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mv /opt/dcos/dcos-metrics-prometheus.env /opt/mesosphere/etc",
+            "sudo mv /opt/dcos/dcos-metrics-prometheus-agent.service /etc/systemd/system",
+            "sudo mv /opt/dcos/dcos-metrics-prometheus-plugin /opt/mesosphere/bin",
+            "sudo chmod 755 /opt/mesosphere/bin/dcos-metrics-prometheus-plugin",
+            "sudo systemctl daemon-reload",
+            "sudo systemctl start dcos-metrics-prometheus-agent"
+        ]
+    }
+
+}
