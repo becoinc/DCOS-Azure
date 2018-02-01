400da36c6c6174639a640b214953876c92570f2e
Jeffrey Zampieron <jzampieron@zproject.net>
2018-02-01 12:08:08 -0500

    Start adding map for AN supported vm types

--- terraform/vm_types.tf
+++ terraform/vm_types.tf
@@ -12,11 +12,39 @@
 
 /* Looks up a Disk type for OS Disks based on the VM type. */
 variable "vm_type_to_os_disk_type" {
-  type    = "map"
-  default = {
-    Standard_A1_v2         = "Standard_LRS",
-    Standard_D2s_v3        = "Premium_LRS",
-    Standard_DS2_v2_Promo  = "Premium_LRS",
-    Standard_DS11_v2_Promo = "Premium_LRS"
-  }
+    type    = "map"
+    default = {
+        Standard_A1_v2         = "Standard_LRS",
+        Standard_D2s_v3        = "Premium_LRS",
+        Standard_DS2_v2_Promo  = "Premium_LRS",
+        Standard_DS11_v2_Promo = "Premium_LRS"
+    }
+}
+
+/**
+ * Define accelerated networking support by VM size.
+ */
+variable "vm_type_to_an" {
+    type = "map"
+
+    /* From: https://docs.microsoft.com/en-us/azure/virtual-network/create-vm-accelerated-networking-cli */
+    default = {
+        Standard_B4ms    = "false"
+        Standard_D8_v3   = "true"
+        Standard_D16_v3  = "true"
+        Standard_D32_v3  = "true"
+        Standard_D64_v3  = "true"
+        Standard_D8s_v3  = "true"
+        Standard_D16s_v3 = "true"
+        Standard_D32s_v3 = "true"
+        Standard_D64s_v3 = "true"
+        Standard_E8_v3   = "true"
+        Standard_E16_v3  = "true"
+        Standard_E32_v3  = "true"
+        Standard_E64_v3  = "true"
+        Standard_E8s_v3  = "true"
+        Standard_E16s_v3 = "true"
+        Standard_E32s_v3 = "true"
+        Standard_E64s_v3 = "true"
+    }
 }
