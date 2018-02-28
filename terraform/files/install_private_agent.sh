e2921d44e1713dea6ac0681964a9e7e559f1b7ba
Jeffrey Zampieron <jzampieron@zproject.net>
2018-02-28 11:02:50 -0500

    Add agentpool attribute

--- terraform/files/install_private_agent.sh
+++ terraform/files/install_private_agent.sh
@@ -7,6 +7,6 @@ mkdir /tmp/dcos
 cd /tmp/dcos
 curl -O http://${BOOTSTRAP_URL}/dcos_install.sh
 sudo bash dcos_install.sh ${ROLE}
-sudo sh -c 'echo MESOS_ATTRIBUTES=pxfabric:pxclust1 >> /var/lib/dcos/mesos-slave-common'
+sudo sh -c 'echo MESOS_ATTRIBUTES=agentpool:default,pxfabric:pxclust1 >> /var/lib/dcos/mesos-slave-common'
 sudo rm -f /var/lib/mesos/slave/meta/slaves/latest
 sudo systemctl restart dcos-mesos-slave.service
